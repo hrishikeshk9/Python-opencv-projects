7f841df2881f599de2b83cd401f503f9c7ab0f68
unknown <hrishikeshhere@gmail.com>
2019-06-13 10:32:01 +0530

    added rectangle for face detection

--- face_recog.py
+++ face_recog.py
@@ -16,8 +16,15 @@ while(True):
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for(x,y,w,h)in faces:
         print(x,y,w,h)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_color = frame[y:y+h,x:x+w]
 
-
+        color = (255,0,0)
+        stroke = 2
+        end_cord_x = x+w
+        end_cord_y = y+h
+        cv2.rectangle(frame,(x,y),(end_cord_x,end_cord_y),color,stroke)
+        
 
 
     cv2.imshow('frame',frame)
